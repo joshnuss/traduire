09428044dd449ba4ce443c4a6850bb4d6b4c9f7d
Josh Nussbaum <joshua.nussbaum@shopify.com>
2017-02-24 21:45:51 -0500

    Extract logic to file

--- lib/traduire.rb
+++ lib/traduire.rb
@@ -1,5 +1,20 @@
 require "traduire/version"
 require 'active_support/all'
+require_relative 'traduire/match'
 
 module Traduire
+  extend self
+
+  def parse(data)
+    return [] unless matches = data.match(/"(.+)"/)
+
+    matches.captures.map do |text|
+      key = text.downcase.gsub(' ', '_').tr('!', '')
+
+      Match.new(string: text,
+                line: 1,
+                suggestion: key,
+                example: data.gsub("\"#{text}\"", "I18n.t(:#{key})"))
+    end
+  end
 end
--- test/traduire_test.rb
+++ test/traduire_test.rb
@@ -2,13 +2,13 @@ require 'test_helper'
 
 class TraduireTest < Minitest::Test
   def test_parses_no_matches
-    matches = parse "puts 123"
+    matches = Traduire.parse "puts 123"
 
     assert_predicate matches, :empty?
   end
 
   def test_parses_file
-    matches = parse 'puts "Hello World!"'
+    matches = Traduire.parse 'puts "Hello World!"'
 
     assert_equal 1, matches.length
 
@@ -18,26 +18,5 @@ class TraduireTest < Minitest::Test
     assert_equal 'Hello World!', match.string
     assert_equal 'hello_world', match.suggestion
     assert_equal 'puts I18n.t(:hello_world)', match.example
-  end
-
-  class Match
-    attr_accessor :line, :string, :suggestion, :example
-
-    def initialize(string:, line:, suggestion:, example:)
-      @string, @line, @suggestion, @example = string, line, suggestion, example
-    end
-  end
-
-  def parse(data)
-    return [] unless matches = data.match(/"(.+)"/)
-
-    matches.captures.map do |text|
-      key = text.downcase.gsub(' ', '_').tr('!', '')
-
-      Match.new(string: text,
-                line: 1,
-                suggestion: key,
-                example: data.gsub("\"#{text}\"", "I18n.t(:#{key})"))
-    end
   end
 end
