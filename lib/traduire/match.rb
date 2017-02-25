75fc33cc96bc325c1c0b0a4b13d210c4cc9fc1b3
Josh Nussbaum <joshua.nussbaum@shopify.com>
2017-02-24 22:16:37 -0500

    Make example parameter optional

--- lib/traduire/match.rb
+++ lib/traduire/match.rb
@@ -1,7 +1,7 @@
 class Match
   attr_accessor :line, :string, :suggestion, :example
 
-  def initialize(string:, line:, suggestion:, example:)
+  def initialize(string:, line:, suggestion:, example: nil)
     @string, @line, @suggestion, @example = string, line, suggestion, example
   end
 end
